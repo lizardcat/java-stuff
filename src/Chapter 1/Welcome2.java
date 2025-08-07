40bb867397a0d6125545a77a7b9f72758f39176e
lizardcat <alex.raza.ke@gmail.com>
2025-08-07 23:51:35 +0300

    Adding Welcome2

---
+++ src/Chapter 1/Welcome2.java
@@ -0,0 +1,7 @@
+public class Welcome2 {
+    // main method begins execution of Java application 
+    public static void main(String[] args) {
+        System.out.println("Welcome to ");
+        System.out.println("Java programming!");
+    } 
+}
